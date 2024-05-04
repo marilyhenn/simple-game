33157124e344df62b3d075c7eb5ee4a9ae492cda
Emily <schroderan@gmail.com>
2024-05-04 16:27:46 +0200

    test

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+a = {
+    "player": 0,
+    "bot" : 0
+}
+
+def even(num: int):
+    if num%2 == 0:
+        return False
+    else:
+        return True
+while True:
+    b = random.randint(100, 1000)
+    if even(b):
+        continue
+    c = random.randint(100, 1000)
+    if even(c):
+        continue
